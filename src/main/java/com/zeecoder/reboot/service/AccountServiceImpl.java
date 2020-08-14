76cbad699e794a7f59aca44dba99363087ed6a97
zeecoder <igorzaporogtsev@gmail.com>
2020-08-14 08:12:00 +0300

    implements CRUD client side

--- src/main/java/com/zeecoder/reboot/service/AccountServiceImpl.java
+++ src/main/java/com/zeecoder/reboot/service/AccountServiceImpl.java
@@ -1,8 +1,13 @@
 
 package com.zeecoder.reboot.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.zeecoder.reboot.model.Account;
+import com.zeecoder.reboot.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +16,7 @@ import java.util.*;
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    private final String url = "http://localhost:8080/account";
     private final RestTemplate restTemplate;
 
     @Autowired
@@ -20,9 +26,51 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public List<Account> getAll() {
-        String url = "http://localhost:8080/account";
         List<Account> accounts = restTemplate.getForObject(url, List.class);
         return accounts;
+    }
+
+    @Override
+    public void add(Account account, String roleStr) {
+        Set<Role> roles = collectRolesToSet(account, roleStr);
+        account.setRoles(roles);
+        restTemplate.postForObject(url + "/add", account, Account.class);
+    }
+
+    @Override
+    public Optional<Account> getOne(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Account account) {
+        restTemplate.put(url + "/update", account, Account.class);
+    }
+
+    @Override
+    public void delete(Long id) {
+        restTemplate.delete(url + "/delete/{id}", id);
+    }
+
+    @Override
+    public Account findByName(String nickname) {
+        Account account = restTemplate.getForObject(url, Account.class);
+        return account;
+    }
+
+    private Set<Role> collectRolesToSet(Account account, String roleStr){
+
+        String[] roleStrings = roleStr.split("\\s*,\\s*");
+        Set<Role> rolesSet = new HashSet<>();
+
+        for (String s: roleStrings) {
+            Role role = new Role();
+            role.setRole(s);
+            role.setAccount(account);
+            rolesSet.add(role);
+        }
+
+        return rolesSet;
     }
 }
 
